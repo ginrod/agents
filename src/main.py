cd0b9a34e1481929570989d7e32784fc2b1b2da9
Ginrod <gilbecubano@gmail.com>
2020-11-14 17:40:54 -0500

    fix implementation bugs

--- src/main.py
+++ src/main.py
@@ -15,15 +15,15 @@ if __name__ == '__main__':
     with open('sim_logs.txt', 'w'): pass
     file = open('sim_logs.txt', 'a')
     
-    agents = [ProactiveAgent(), ReactiveAgent()]
+    agents = [ProactiveAgent, ReactiveAgent]
     sim_num = 1
     for r_num, agent in enumerate(agents):
         environments = [create_initial_environment(agent) for _ in range(10)]
         for e_num, environment in enumerate(environments):
-            register_msg(f"#Simulacion {sim_num}\n")
-            register_msg(f"#Robot de tipo {r_num}\n")
-            register_msg(f"#Ambiente {e_num + 1}")
-            run_simulation(env)
+            register_msg(f"#Simulacion {sim_num}\n", file)
+            register_msg(f"#Robot de tipo {r_num}\n", file)
+            register_msg(f"#Ambiente {e_num + 1}", file)
+            run_simulation(environment)
             sim_num += 1
     
     file.close()
