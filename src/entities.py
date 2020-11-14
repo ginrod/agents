97a8ac7a79e8b008f65d4fc0a3d5fefa9e015dba
Ginrod <gilbecubano@gmail.com>
2020-11-14 16:18:11 -0500

    add directions

--- src/entities.py
+++ src/entities.py
@@ -1,3 +1,40 @@
+class Direction:
+
+    @staticmethod
+    def north():
+        return 1, 0
+    
+    @staticmethod
+    def north_east():
+        return -1, 1
+    
+    @staticmethod
+    def east():
+        return 0, 1
+    
+    @staticmethod
+    def south_east():
+        return 1, 1
+    
+    @staticmethod
+    def south():
+        return 1, 0
+    
+    @staticmethod
+    def south_west():
+        return 1, -1
+    
+    @staticmethod
+    def west():
+        return 0, -1
+    
+    @staticmethod
+    def north_west():
+        return -1, -1
+
+directions = [Direction.north(), Direction.north_east(), Direction.east(), Direction.south_east(), 
+              Direction.south(), Direction.south_west(), Direction.west(), Direction.north_west()]
+
 class EnvironmentElement:
 
     def __init__(self, x, y, environment):
@@ -14,8 +51,8 @@ class Obstacle(EnvironmentElement):
 class Dirt(EnvironmentElement):
     pass
 
-class Child(EnvironmentElement):
+class Playpen(EnvironmentElement):
     pass
 
-class Playpen(EnvironmentElement):
+class Child(EnvironmentElement):
     pass
