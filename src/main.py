3f79e4cdb1913dea3c8c560e51f7987677222bab
Ginrod <gilbecubano@gmail.com>
2020-11-14 17:39:35 -0500

    setting a more complete infrastructure

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
 from entities import *
 from agents import *
 
+def register_msg(msg, file):
+    print(msg, end="")
+    file.write(msg)
+
+def create_initial_environment(agent):
+    pass
+
+def run_simulation(env):
+    pass
+
 if __name__ == '__main__':
+    with open('sim_logs.txt', 'w'): pass
+    file = open('sim_logs.txt', 'a')
+    
     agents = [ProactiveAgent(), ReactiveAgent()]
-    for agent in agents:
-        # Create environments and run simulations
-        pass
+    sim_num = 1
+    for r_num, agent in enumerate(agents):
+        environments = [create_initial_environment(agent) for _ in range(10)]
+        for e_num, environment in enumerate(environments):
+            register_msg(f"#Simulacion {sim_num}\n")
+            register_msg(f"#Robot de tipo {r_num}\n")
+            register_msg(f"#Ambiente {e_num + 1}")
+            run_simulation(env)
+            sim_num += 1
+    
+    file.close()
